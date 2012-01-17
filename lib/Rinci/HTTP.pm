c0de93fc2452fa1af41cab97739db662a5277fa5
Steven Haryanto <stevenharyanto@gmail.com>
2012-01-17 05:55:27 +0000

    Import of SHARYANTO/Rinci-HTTP-1.1.0 from CPAN.
    gitpan-cpan-distribution: Rinci-HTTP
    gitpan-cpan-version:      1.1.0
    gitpan-cpan-path:         SHARYANTO/Rinci-HTTP-1.1.0.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Rinci/HTTP.pm
@@ -0,0 +1,31 @@
+package Rinci::HTTP;
+
+our $VERSION = '1.1.0'; # VERSION
+
+1;
+# ABSTRACT: Request metadata and perform actions on code entities over HTTP
+
+__END__
+=pod
+
+=head1 NAME
+
+Rinci::HTTP - Request metadata and perform actions on code entities over HTTP
+
+=head1 VERSION
+
+version 1.1.0
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
